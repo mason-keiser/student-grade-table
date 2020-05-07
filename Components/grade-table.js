77205f7d8e0d6544e92643d67b40c451626d4023
Mason Keiser <Masonksr5@gmail.com>
2020-05-07 16:46:50 -0700

    view grades changes

--- Components/grade-table.js
+++ Components/grade-table.js
@@ -5,16 +5,16 @@ class GradeTable {
     updateGrades(grades) {
         var treeBody = this.tableElement.querySelector("tbody");
             treeBody.innerHTML = "";
-                for(var i = 0; i < grades.length; i++) {
-                    var newTreeRow = document.createElement("tr");
-                    var studentName = document.createElement("td");
-                    var studentCourse = document.createElement("td");
-                    var studentGrade = document.createElement("td");
-                        studentName.textContent = grades[i].name;
-                        studentCourse.textContent = grades[i].course;
-                        studentGrade.textContent = grades[i].grade;
-                            newTreeRow.append(studentName, studentCourse, studentGrade);
-                                treeBody.appendChild(newTreeRow);
-                }
+            for(var i = 0; i < grades.length; i++) {
+                var newTreeRow = document.createElement("tr");
+                var studentName = document.createElement("td");
+                var studentCourse = document.createElement("td");
+                var studentGrade = document.createElement("td");
+                studentName.textContent = grades[i].name;
+                studentCourse.textContent = grades[i].course;
+                studentGrade.textContent = grades[i].grade;
+                newTreeRow.append(studentName, studentCourse, studentGrade);
+                treeBody.appendChild(newTreeRow);
+            }
     }
 }
