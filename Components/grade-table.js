33c3396e28256ae9348720b22c283ab0a54a3741
Mason Keiser <Masonksr5@gmail.com>
2020-05-07 16:03:17 -0700

    added ability to view grades

---
+++ Components/grade-table.js
@@ -0,0 +1,20 @@
+class GradeTable {
+    constructor(tableElement) {
+        this.tableElement = tableElement;
+    }
+    updateGrades(grades) {
+        var treeBody = this.tableElement.querySelector("tbody");
+            treeBody.innerHTML = "";
+                for(var i = 0; i < grades.length; i++) {
+                    var newTreeRow = document.createElement("tr");
+                    var studentName = document.createElement("td");
+                    var studentCourse = document.createElement("td");
+                    var studentGrade = document.createElement("td");
+                        studentName.textContent = grades[i].name;
+                        studentCourse.textContent = grades[i].course;
+                        studentGrade.textContent = grades[i].grade;
+                            newTreeRow.append(studentName, studentCourse, studentGrade);
+                                treeBody.appendChild(newTreeRow);
+                }
+    }
+}
--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-//create user ajax function
-//$.ajax({
-//    url: 'https://sgt.lfzprototypes.com/api/users',
-//    method: "POST",
-//    data: {
-//        firstName: "Mason",
-//       lastName: "Keiser",
-//        cohort: "c04.20"
-//    },
-//    success: function (response) {
-//        console.log(response)
-//    }
-//})
+var tableElement = document.querySelector('table');
+var newGradeTable = new GradeTable(tableElement);
+var newApp = new App(newGradeTable);
+    newApp.start();
